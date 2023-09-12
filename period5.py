e4e426bac677693360ec5e4a8c45d6b8d2d491e9
AzerbijanFan Foenem <martingoldenlion@gmail.com>
2023-09-12 20:13:15 +0000

    Period 5 In Class Practice Task

---
+++ period5.py
@@ -0,0 +1,22 @@
+restaurants = ["Mcdonalds", "WingStop", "Baja Cali"]
+
+new_res = input("what restaurant would you like to rank in your list")
+
+def rank_restaurant(new_res, restaurants):
+
+    for i in range (len(restaurants)):
+
+        rank = input ("Do you like A)" + new_res + "more or B)" + restaurants[i] + "more? A/B")
+
+        if rank == "A":
+            restaurants.insert(i, new_res)
+            break
+        elif rank == "B":
+            continue
+    
+    if new_res not in restaurants:
+        restaurants.append(new_res)
+
+    return restaurants
+    print ("Your new ranking of restaurants is", rank_restaurant(new_res, restaurants))
+
